ba23a84206e6caa6b30e8b04d667ae05ce019c38
HelpAStranger <134454719+HelpAStranger@users.noreply.github.com>
2024-07-12 19:03:00 -0300

    7.0.10
    Novos itens para o mekamod

---
+++ kubejs/startup_scripts/mekamodupdate.js
@@ -0,0 +1,21 @@
+StartupEvents.registry('item', event => { 
+    event.create('mekamod:amarelum')
+    .displayName('§eAmarelum')
+	.maxStackSize(64)
+	
+    event.create('mekamod:aquam')
+    .displayName('§bAquam')
+	.maxStackSize(64)
+
+    event.create('mekamod:rosum')
+    .displayName('§dRosum')
+	.maxStackSize(64)
+
+    event.create('mekamod:verdum')
+    .displayName('§aVerdum')
+	.maxStackSize(64)
+
+    event.create('mekamod:laranjum')
+    .displayName('§6Laranjum')
+	.maxStackSize(64)
+})
