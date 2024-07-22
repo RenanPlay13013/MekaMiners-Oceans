ab8fc3143ff4c4f6f158591dd8bd1c1d6d6ad4af
HelpAStranger <134454719+HelpAStranger@users.noreply.github.com>
2024-07-22 18:07:53 -0300

    Removido server_scripts

--- kubejs/server_scripts/Tags.js
+++
@@ -1,8 +0,0 @@
-ServerEvents.tags('item', event => {
-
-//Tornando MekaHammer uma Wrench
-const tags = ['forge:tools/wrench', 'forge:tools/wrenches']
-tags.forEach(TAGS =>{ 
-event.add(`${TAGS}`,'mekamod:meka_paxel')
-})
-})
